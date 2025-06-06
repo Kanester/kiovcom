4ec79df26a41340df900b7268736f6fb166d1df0
KanesterP <pampagkieven091@gmail.com>
2025-06-06 15:23:50 +0800

    --no-message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import { dev } from "$app/environment";
   import { injectAnalytics } from "@vercel/analytics/sveltekit";
+  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
   
   injectAnalytics({
     mode: dev ? 'development' : 'production'
   });
+  injectSpeedInsights();
 </script>
 
 <slot/>
